b81ad54ef5d21e4d82713218b15a3cf70bad119f
sateesh <sateeshnaidu900@gmail.com>
2021-03-03 17:53:27 +0530

    initial commit

---
+++ MostUsefulJSFeaures.js
@@ -0,0 +1,24 @@
+
+// Most Useful JS Features used 
+/*
+1.let and const;
+2: Shorthand Object Assignment,
+3: Destructuring Assignment,
+4: Spread Syntax,
+5: Optional Chaining,
+6: Nullish Coalescing Operator,
+7: Ternary Operator,
+8: Template Literals,
+9: Arrow Functions,
+10: Import and Export
+*/
+
+// 1.let and const
+let name="sateesh";
+function fun(){
+  let name="MS";
+  console.log(name);// Prints MS 
+}
+console.log(name); // Print sateesh
+let name="Dravid";
+console.log(name);// Prints Dravid
